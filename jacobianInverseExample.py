8d10798f207b00b5e5928a05594955efaf9a3cec
damaximum <amaxsein@masonlive.gmu.edu>
2019-03-04 16:48:56 -0500

    I beleive working IK

--- jacobianInverseExample.py
+++ jacobianInverseExample.py
@@ -1,26 +1,21 @@
 import numpy
 import math
+import forwardKinematics
+
+UpperArmLength  =105.0
+ElbowOffsetY    =15.0
+LowerArmLength  =55.95
+HandOffsetX 	=57.75
+ShoulderOffsetY =98.0
+HandOffsetZ     =12.31
 
 incrementVal = .001
 l0,l1,l2=1,1.2,1.5
-theta0,theta1,theta2=45,30,80 # starting theta
+theta0,theta1,theta2=45,30,40 # starting theta
 thetaOld = numpy.array([theta0,theta1,theta2])
 startingPoint = 0,0,0
-desiredPoint = 1.2,1.6,1.1 # meters
+desiredPoint = 1.7,1.6,1.2 # meters
 b=0.0 # just place holder
-
-# need to update the slope
-if((desiredPoint[1]/desiredPoint[0]) > 1):
-	newY = .001
-	newX = (newY-b)/(desiredPoint[1]/desiredPoint[0])
-else:
-	newX = .001
-	newY = (desiredPoint[1]/desiredPoint[0])*newX + b
-
-newPos = numpy.array([newX,newY,newZ])
-
-
-print "Next Position incrementally going to: ",newPos
 
 def find2dSlope(des,cur):
 	slope = (des[1]-cur[1])/(des[0]-cur[0])
@@ -30,6 +25,12 @@ def find3dSlope(des,cur):
 	xDiff = math.sqrt(math.pow(des[0]-cur[0],2) + math.pow(des[1]-cur[0],2))
 	yDiff = des[2]-cur[2]
 	return math.sqrt(math.pow(xDiff,2)+math.pow(yDiff,2))
+
+def find3dDistance(des,cur):
+	xPrime = math.sqrt(math.pow(des[0]-cur[0],2)+math.pow(des[1]-cur[1],2))
+	res = math.sqrt(math.pow(xPrime,2)+math.pow(desiredPoint[2]-cur[2],2))
+	print res
+	return res
 
 #print(find3dSlope(desiredPoint,startingPoint))
 
@@ -71,36 +72,40 @@ def matMullTransformation(listOTs):
 
 joint0 = createTransformation([l0,0,0],theta0,'z')
 joint1 = createTransformation([l1,0,0],theta1,'z')
-joint2 = createTransformation([l2,0,0],theta2,'x')
+joint2 = createTransformation([0,0,l2],theta2,'y')
 base = matMullTransformation([joint0,joint1,joint2])
-print base
-startingPoint = base[0,3],base[1,3]
-print startingPoint
+#print base
+startingPoint = numpy.zeros(3)
+startingPoint[0] = base[0,3]
+startingPoint[1] = base[1,3]
+startingPoint[2] = base[2,3]
+print "starting",startingPoint
+print "desired",desiredPoint
 def createJacobian():
 	global joint0,joint1,joint2
 	dTheta = .001 # degrees
 	theta0Prime = theta0 + dTheta
 	theta1Prime = theta1 + dTheta
 	theta2Prime = theta2 + dTheta
-
+	#forwardKinematics.createTransforms([],"RArm")
 	PPrime0 = createTransformation([l0,0,0],(theta0Prime),'z')
 	PPrime1 = createTransformation([l1,0,0],theta1,'z')
-	PPrime2 = createTransformation([l2,0,0],theta2,'x')
+	PPrime2 = createTransformation([0,0,l2],theta2,'y')
 
-	basePrime = matMullTransformation([PPrime0,PPrime1])
-	print("theta0Prime:")
-	print(basePrime)
+	basePrime = matMullTransformation([PPrime0,PPrime1,PPrime2])
+	#print("theta0Prime:")
+	#print(basePrime)
 	dexdTheta0 = (basePrime[0,3] - base[0,3])/dTheta
 	deydTheta0 = (basePrime[1,3] - base[1,3])/dTheta
 	dezdTheta0 = (basePrime[2,3] - base[2,3])/dTheta
 
 	PPrime0 = createTransformation([l0,0,0],theta0,'z')
 	PPrime1 = createTransformation([l1,0,0],(theta1Prime),'z')
-	PPrime2 = createTransformation([l2,0,0],theta2,'x')
+	PPrime2 = createTransformation([0,0,l2],theta2,'y')
 
-	basePrime = matMullTransformation([PPrime0,PPrime1])
-	print("theta1Prime:")
-	print(basePrime)
+	basePrime = matMullTransformation([PPrime0,PPrime1,PPrime2])
+	#print("theta1Prime:")
+	#print(basePrime)
 	dexdTheta1 = (basePrime[0,3] - base[0,3])/dTheta
 	deydTheta1 = (basePrime[1,3] - base[1,3])/dTheta
 	dezdTheta1 = (basePrime[2,3] - base[2,3])/dTheta
@@ -108,11 +113,11 @@ def createJacobian():
 
 	PPrime0 = createTransformation([l0,0,0],theta0,'z')
 	PPrime1 = createTransformation([l1,0,0],theta1,'z')
-	PPrime2 = createTransformation([0,0,l1],(theta2Prime),'x')
+	PPrime2 = createTransformation([0,0,l2],(theta2Prime),'y')
 
 	basePrime = matMullTransformation([PPrime0,PPrime1,PPrime2])
-	print("theta2Prime:")
-	print(basePrime)
+	#print("theta2Prime:")
+	#print(basePrime)
 	dexdTheta2 = (basePrime[0,3] - base[0,3])/dTheta
 	deydTheta2 = (basePrime[1,3] - base[1,3])/dTheta
 	dezdTheta2 = (basePrime[2,3] - base[2,3])/dTheta
@@ -127,51 +132,90 @@ def createJacobian():
 	#J = numpy.array([[dexdTheta0,dexdTheta1]#,dexdTheta2],
 	#		[deydTheta0, deydTheta1]])#,deydTheta2]])
 
-	print(J)
+	#print(J)
 	return J
 
 J = createJacobian()
 Jinverse = numpy.linalg.inv(J)
-print Jinverse
-#print(newPos)
+#print Jinverse
+newPos = startingPoint
 for a in range(2000):
-	slope = find2dSlope(desiredPoint,newPos)#(desiredPoint[1]-newPos[1])/(desiredPoint[0]-newPos[0])
+	diffY,diffX = (desiredPoint[1]-newPos[1]),(desiredPoint[0]-newPos[0])
+	slope = abs(find2dSlope(desiredPoint,newPos))#(desiredPoint[1]-newPos[1])/(desiredPoint[0]-newPos[0])
+	diffZ = desiredPoint[2]-newPos[2]
+	if(max([diffX,diffY,diffZ]) == diffY):
+		if(diffY >0):
+			newY = .001
+		else:
+			newY = -.001
+		if(diffX >0):
+			newX = (newY-b)/(slope)
+		else:
+			newX = -(newY-b)/(slope)
+		distTemp = math.sqrt(math.pow(newX,2)+math.pow(newY,2))
+		if(diffZ>0):
+			newZ = math.sqrt(math.pow(distTemp,2) + math.pow(diffZ,2))
+		else:
+			newZ = -math.sqrt(math.pow(distTemp,2) + math.pow(diffZ,2))
 
-	if(slope > 1):
-		newY = .001
-		newX = (newY-b)/(slope)
-	elif(slope <= 1 and slope >0):
-		newX = .001
-		newY = (slope)*newX + b
-	elif(slope < 0 and slope > -1):
-		newX = -.001
-		newY = (slope)*newX + b
-	elif(slope < -1):
-		newY = -.001
-		newX = (newY-b)/(slope)
+	elif(max([diffX,diffY,diffZ]) == diffX):
+		if(diffX >0):
+			newX = .001
+		else:
+			newX = -.001
+		if(diffY > 0):
+			newY = (slope)*newX + b
+		else:
+			newY = -(slope)*newX + b
+		distTemp = math.sqrt(math.pow(newX,2)+math.pow(newY,2))
+		if(diffZ>0):
+			newZ = math.sqrt(math.pow(distTemp,2) + math.pow(diffZ,2))
+		else:
+			newZ = -math.sqrt(math.pow(distTemp,2) + math.pow(diffZ,2))
+	else:
+		print("yeet")
+		if(diffZ > 0):
+			newZ = .001
+		else:
+			newZ = -.001
+		if(diffX >0):
+			newX = .001
+		else:
+			newX = -.001
+		if(diffY > 0):
+			newY = (slope)*newX + b
+		else:
+			newY = -(slope)*newX + b
+
+
+	"""if(diffZ >0.0):
+		newZ = .001
+	else:
+		newZ = -.001"""
 
 	# need to figure out newZ
 
-	print newX, newY
 	newPos[0] = newX # - newPos[0]
 	newPos[1] = newY # - newPos[1]
 	newPos[2] = newZ
-	print newPos
+	print "movement",newPos
 	dThetaJ = numpy.matmul(Jinverse,newPos)
-	print "change in theta", dThetaJ
+	#print "change in theta", dThetaJ
 	thetaNew = thetaOld + dThetaJ
-	print thetaNew
+	#print thetaNew
 	joint0 = createTransformation([l0,0,0],thetaNew[0],'z')
 	joint1 = createTransformation([l1,0,0],thetaNew[1],'z')
-	#joint2 = createTransformation([l2,0,0],theta2,'z')
-	baseP = matMullTransformation([joint0,joint1])
-
-	newP = numpy.zeros(2)
+	joint2 = createTransformation([0,0,l2],thetaNew[2],'y')
+	baseP = matMullTransformation([joint0,joint1,joint2])
+	#print(baseP)
+	newP = numpy.zeros(3)
 	newP[0] = baseP[0,3] # set the x
 	newP[1] = baseP[1,3] # set the y
+	newP[2] = baseP[2,3] # set the z
 	print "new points",newP
-	if(math.sqrt(math.pow(desiredPoint[0]-newP[0],2)+math.pow(desiredPoint[1]-newP[1],2))<.01):
+	if(find3dDistance(desiredPoint,newP)<.01):
 		print "found it at: ",a
+		print thetaNew
 		break
 	newPos = newP
 	thetaOld = thetaNew
