51be1264cc20595812b269b9c535dff1238ca813
damaximum <amaxsein@masonlive.gmu.edu>
2019-03-19 12:13:44 -0400

    script to process csvs

---
+++ processBatdata.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+time = []
+data = []
+
+with open("/home/alex/Documents/ECE498/naoBasicKinematics/testTimingPart1.csv") as f:
+    for line in f:
+        dat = line.split(",")
+        time.append(float(dat[0]))
+        data.append(float(dat[1]))
+lastTime = time[len(time)-1]
+print(lastTime)
+with open("/home/alex/Documents/ECE498/naoBasicKinematics/testTimingpart2.csv") as f:
+    for line in f:
+        dat = line.split(",")
+        time.append(float(dat[0])+lastTime)
+        data.append(float(dat[1]))
+lastTime2 = time[len(time)-1]
+print(lastTime2)
+with open("/home/alex/Documents/ECE498/naoBasicKinematics/testTiming3.csv") as f:
+    for line in f:
+        dat = line.split(",")
+        time.append(float(dat[0])+lastTime2)
+        data.append(float(dat[1]))
+plt.figure()
+plt.title('battery% vs time(sec)')
+plt.plot(time,data)
+#plt.plot(range(len(altData)),altData)
+plt.show()
