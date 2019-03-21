0d1bc4c34bdb0857acad05f8d404908252d834f8
Alex Maxseiner <amaxsein@masonlive.gmu.edu>
2019-03-21 14:12:36 -0400

    cleaned up csvs

--- processBatdata.py
+++ processBatdata.py
@@ -4,25 +4,26 @@ import matplotlib.pyplot as plt
 time = []
 data = []
 
-with open("/home/alex/Documents/ECE498/naoBasicKinematics/testTimingPart1.csv") as f:
+with open("/home/maxseiner/projects/naoBasicKinematics/testTiming1.csv") as f:
     for line in f:
         dat = line.split(",")
         time.append(float(dat[0]))
         data.append(float(dat[1]))
 lastTime = time[len(time)-1]
 print(lastTime)
-with open("/home/alex/Documents/ECE498/naoBasicKinematics/testTimingpart2.csv") as f:
+with open("/home/maxseiner/projects/naoBasicKinematics/testTiming2.csv") as f:
     for line in f:
         dat = line.split(",")
         time.append(float(dat[0])+lastTime)
         data.append(float(dat[1]))
 lastTime2 = time[len(time)-1]
 print(lastTime2)
-with open("/home/alex/Documents/ECE498/naoBasicKinematics/testTiming3.csv") as f:
+"""with open("/home/alex/Documents/ECE498/naoBasicKinematics/testTiming3.csv") as f:
     for line in f:
         dat = line.split(",")
         time.append(float(dat[0])+lastTime2)
         data.append(float(dat[1]))
+"""
 plt.figure()
 plt.title('battery% vs time(sec)')
 plt.plot(time,data)
